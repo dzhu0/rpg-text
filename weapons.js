dac246e3d61301a74fc1015195818ea60b95af58
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2024-01-17 21:03:24 -0600

    Update weapons.js
    Add IDs to all of the weapons.

--- weapons.js
+++ weapons.js
@@ -1,17 +1,21 @@
 export default [
     {
+        id: 1,
         name: "stick",
         power: 5
     },
     {
+        id: 2,
         name: "dagger",
         power: 30
     },
     {
+        id: 3,
         name: "claw hammer",
         power: 50
     },
     {
+        id: 4,
         name: "sword",
         power: 100
     }
