4fa00af68b3a46267c06bc11f02bcbd35cb57108
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2024-01-20 18:20:27 -0600

    Create player.js
    Player's initial stats, gold, and inventory.

---
+++ player.js
@@ -0,0 +1,12 @@
+export default {
+    xp: 0,
+    health: 100,
+    gold: 50,
+    inventory: [
+        {
+            id: 1,
+            name: "Stick",
+            power: 5
+        }
+    ]
+}
