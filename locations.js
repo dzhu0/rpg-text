f68e736d5a3d86df2968d50de2427e45dc8875df
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2024-01-23 17:02:15 -0600

    Update locations.js
    Modify locations to have an array of objects where each object is a location. Each location has a commands array with three objects where it may contain text, action, location, or monster.

--- locations.js
+++ locations.js
@@ -1,52 +1,172 @@
-import * as f from "./script.js"
-
-export default {
-    "town square": {
-        name: "town square",
-        "button text": ["Go to store", "Go to cave", "Fight dragon"],
-        "button functions": [f.goStore, f.goCave, f.fightDragon],
-        text: "You are in the town square. You see a sign that says \"Store\"."
+export default [
+    {
+        id: 0,
+        name: "Start",
+        commands: [
+            {
+                text: "Go to Store",
+                action: "GO_TO",
+                location: 2
+            },
+            {
+                text: "Go to Cave",
+                action: "GO_TO",
+                location: 3
+            },
+            {
+                text: "Fight Dragon",
+                action: "FIGHT",
+                location: 4,
+                monster: 3
+            }
+        ],
+        text: "Welcome to Dragon Repeller. You must defeat the Dragon that is preventing people from leaving the town. You are in the Town Square. Where do you want to go? Use the buttons above."
     },
-    "store": {
-        name: "store",
-        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-        "button functions": [f.buyHealth, f.buyWeapon, f.goTown],
-        text: "You enter the store."
+    {
+        id: 1,
+        name: "Town Square",
+        commands: [
+            {
+                text: "Go to Store",
+                action: "GO_TO",
+                location: 2
+            },
+            {
+                text: "Go to Cave",
+                action: "GO_TO",
+                location: 3
+            },
+            {
+                text: "Fight Dragon",
+                action: "FIGHT",
+                location: 4,
+                monster: 3
+            }
+        ],
+        text: "You are in the Town Square. You see a sign that says \"Store\"."
     },
-    "cave": {
-        name: "cave",
-        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-        "button functions": [f.fightSlime, f.fightBeast, f.goTown],
-        text: "You enter the cave. You see some monsters."
+    {
+        id: 2,
+        name: "Store",
+        commands: [
+            {
+                text: "Buy 10 health (10 gold)",
+                action: "BUY_HEALTH"
+            },
+            {
+                text: "Buy weapon (30 gold)",
+                action: "BUY_WEAPON"
+            },
+            {
+                text: "Go to Town Square",
+                action: "GO_TO",
+                location: 1
+            }
+        ],
+        text: "You enter the Store."
     },
-    "fight": {
-        name: "fight",
-        "button text": ["Attack", "Dodge", "Run"],
-        "button functions": [f.attack, f.dodge, f.goTown],
+    {
+        id: 3,
+        name: "Cave",
+        commands: [
+            {
+                text: "Fight Slime",
+                action: "FIGHT",
+                location: 4,
+                monster: 1
+            },
+            {
+                text: "Fight Fanged Beast",
+                action: "FIGHT",
+                location: 4,
+                monster: 2
+            },
+            {
+                text: "Go to Town Square",
+                action: "GO_TO",
+                location: 1
+            }
+        ],
+        text: "You enter the Cave. You see some monsters."
+    },
+    {
+        id: 4,
+        name: "Fight",
+        commands: [
+            {
+                text: "Attack",
+                action: "ATTACK"
+            },
+            {
+                text: "Dodge",
+                action: "DODGE"
+            },
+            {
+                text: "Run",
+                action: "GO_TO",
+                location: 1
+            }
+        ],
         text: "You are fighting a monster."
     },
-    "kill monster": {
-        name: "kill monster",
-        "button text": ["Go to town square", "Go to town square", "Go to town square"],
-        "button functions": [f.goTown, f.goTown, f.easterEgg],
-        text: "The monster screams \"Arg!\" as it dies. You gain experience points and find gold."
+    {
+        id: 5,
+        name: "Kill Monster",
+        commands: [
+            {
+                text: "Go to Town Square",
+                action: "GO_TO",
+                location: 1
+            },
+            {
+                text: "Go to Town Square",
+                action: "GO_TO",
+                location: 1
+            },
+            {
+                text: "Go to Town Square",
+                action: "GO_TO",
+                location: 1
+            }
+        ],
+        text: "The monster screams \"ARG!\" as it dies. You gain experience points and find gold."
     },
-    "lose": {
-        name: "lose",
-        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
-        "button functions": [f.restart, f.restart, f.restart],
+    {
+        id: 6,
+        name: "Lose",
+        commands: [
+            {
+                text: "REPLAY?",
+                action: "RESTART"
+            },
+            {
+                text: "REPLAY?",
+                action: "RESTART"
+            },
+            {
+                text: "REPLAY?",
+                action: "RESTART"
+            }
+        ],
         text: "You die."
     },
-    "win": {
-        name: "win",
-        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
-        "button functions": [f.restart, f.restart, f.restart],
-        text: "You defeat the dragon! YOU WIN THE GAME!"
-    },
-    "easter egg": {
-        name: "easter egg",
-        "button text": ["2", "8", "Go to town square?"],
-        "button functions": [f.pickTwo, f.pickEight, f.goTown],
-        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
+    {
+        id: 7,
+        name: "Win",
+        commands: [
+            {
+                text: "REPLAY?",
+                action: "RESTART"
+            },
+            {
+                text: "REPLAY?",
+                action: "RESTART"
+            },
+            {
+                text: "REPLAY?",
+                action: "RESTART"
+            }
+        ],
+        text: "You defeat the Dragon! YOU WIN THE GAME!"
     }
-}
+]
